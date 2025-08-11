c8fa39e32c97e6a4af94f37216f335f8eaba06b8
k33g <ph.charriere@gmail.com>
2025-08-11 15:17:59 +0200

    📦 🦅 osprey v0.1.0: added mcp resources with templates support

--- lib/osprey.sh
+++ lib/osprey.sh
@@ -7,7 +7,7 @@ COMMENT
 
 function osprey_version() {
     echo "Osprey - A Bash library for interacting with the DMR API"
-    echo "Version: v0.0.9"
+    echo "Version: v0.1.0"
     echo "Author: k33g"
     echo "License: unlicense"
 }
